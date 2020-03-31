35fac171ade5b29c874da320d5f42a9568c030dc
Zrgisa <sgrivcov@godaddy.com>
2020-03-31 19:46:25 +0200

    download button appearing

---
+++ download-image.js
@@ -0,0 +1,146 @@
+const el = wp.element.createElement;
+const { __ } = wp.i18n;
+
+const addDownloadButtonAttributes = ( settings, name ) => {
+	if ( name !== 'coblocks/gallery-masonry' ) {
+		return settings;
+	}
+
+	settings.attributes = lodash.assign( settings.attributes, {
+		downloadEnabled: {
+			type: 'boolean',
+			default: false,
+		},
+		downloadAllEnabled: {
+			type: 'boolean',
+			default: false,
+		},
+	} );
+
+	return settings;
+};
+
+const downloadControl = wp.compose.createHigherOrderComponent( function( BlockEdit ) {
+	return function( props ) {
+		if ( props.name !== 'coblocks/gallery-masonry' ) {
+			return el(
+				BlockEdit,
+				props
+			);
+		}
+
+		const { downloadEnabled, downloadAllEnabled } = props.attributes;
+
+		return el(
+			wp.element.Fragment,
+			{},
+			el(
+				BlockEdit,
+				props
+			),
+			el(
+				wp.blockEditor.InspectorControls,
+				{},
+				el(
+					wp.components.PanelBody,
+					{
+						title: 'Download Buttons',
+						initialOpen: false
+					},
+					el(
+						wp.components.ToggleControl,
+						{
+							label: 'Download Single Image',
+							checked: downloadEnabled,
+							onChange: function () {
+								props.setAttributes( {
+									downloadEnabled: !downloadEnabled,
+								} );
+							}
+						}
+					),
+					el(
+						wp.components.ToggleControl,
+						{
+							label: 'Download All',
+							checked: downloadAllEnabled,
+							onChange: function () {
+								props.setAttributes( {
+									downloadEnabled: !downloadAllEnabled,
+								} );
+							}
+						}
+					)
+				)
+			)
+		);
+	};
+}, 'withDownloadControls' );
+
+const addDownloadButton = ( element, blockType, attributes ) => {
+	if ( blockType.name !== 'coblocks/gallery-masonry' || !attributes.downloadEnabled ) {
+		return element;
+	}
+
+	let listElement = element;
+	let tree = [];
+	while (listElement != null && listElement.type !== 'ul')
+	{
+		tree.push(listElement);
+		listElement = React.Children.toArray(listElement.props.children)[0];
+	}
+
+	if (listElement === null)
+	{
+		return element;
+	}
+
+	const elements = React.Children.toArray(listElement.props.children);
+	let newElements = [];
+
+	for (let i = 0; i < elements.length; ++i)
+	{
+		let children = React.Children.toArray(elements[i].props.children);
+		let shouldAdd = true;
+
+		for (let j = 0; j < children.length; ++j) {
+			if (children[j].type === 'span') {
+				shouldAdd = false;
+			}
+		}
+
+		if (shouldAdd) {
+			children.push(
+				el(
+					'a',
+					{
+						href: 'https://google.com',
+						target: '_blank',
+						rel: 'noopener noreferrer'
+					},
+					'Download'
+				)
+			);
+		}
+
+		newElements.push(React.cloneElement(
+			elements[i],
+			{},
+			...children
+		));
+	}
+
+	let lastElement = React.cloneElement(listElement, {}, ...newElements);
+	let k = tree.length - 1;
+
+	while (k >= 0) {
+		lastElement = React.cloneElement(tree[k], {}, lastElement);
+		--k;
+	}
+
+	return lastElement;
+};
+
+wp.hooks.addFilter( 'editor.BlockEdit', 'download-image/download-controls', downloadControl );
+wp.hooks.addFilter( 'blocks.registerBlockType', 'download-image/attribute/download', addDownloadButtonAttributes );
+wp.hooks.addFilter( 'blocks.getSaveElement', 'download-image/add-download-button', addDownloadButton );
--- download-image.php
+++ download-image.php
@@ -8,3 +8,13 @@
  * Text Domain: download-image
  */
 
+function download_image_enqueue() {
+    wp_enqueue_script(
+        'download-image-script',
+        plugins_url( 'download-image.js', __FILE__ ),
+        array( 'wp-blocks', 'wp-dom-ready', 'wp-edit-post', 'lodash' ),
+        filemtime( plugin_dir_path( __FILE__ ) . '/download-image.js' )
+    );
+}
+
+add_action( 'enqueue_block_editor_assets', 'download_image_enqueue' );
