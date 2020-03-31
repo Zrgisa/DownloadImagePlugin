65c6530b71c68a6d2fa21a03299571df5404fa23
Zrgisa <sgrivcov@godaddy.com>
2020-03-31 13:44:39 +0200

    base plugin file

---
+++ download-image.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: Download Image Extension
+ * Plugin URI: https://github.com/Zrgisa/DownloadImagePlugin
+ * Description: An extension for image downloads inside the Masonry Gallery block.
+ * Version: 0.1
+ * Author: GoDaddy
+ * Text Domain: download-image
+ */
+
